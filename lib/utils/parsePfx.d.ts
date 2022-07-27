252e82aa89a176070992686eb20517cb10152163
付雪双 <fuxueshuang@qianxin.com>
2022-07-27 17:56:30 +0800

    gitignore

--- lib/utils/parsePfx.d.ts
+++
@@ -1,8 +0,0 @@
-interface Params {
-    path: string;
-    password: string;
-}
-declare function getAttributes(params: Params): Promise<any>;
-declare function getPrivateKey(params: Params): Promise<any>;
-declare function getCertificate(params: Params): Promise<any>;
-export { getAttributes, getPrivateKey, getCertificate };
