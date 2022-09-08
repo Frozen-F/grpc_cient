0fcd997e151253e322de15d18bbd0cce1969bd23
付雪双 <fuxueshuang@qianxin.com>
2022-09-08 16:01:03 +0800

    fix:openssl替换

--- src/utils/pfxUtils.ts
+++ src/utils/pfxUtils.ts
@@ -1,23 +1,24 @@
-import pem, { Pkcs12ReadResult } from 'pem';
+import * as p12 from 'p12-pem';
+// import pem, { Pkcs12ReadResult } from 'pem';
 
-const parsePfxUtil = (path:string, password:string):Promise<Pkcs12ReadResult>=>{
-  return new Promise((resolve, reject)=>{
-    pem.readPkcs12(path, { p12Password: password }, (err:any, res:Pkcs12ReadResult) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(res);
-    });
-  });
-};
+// const parsePfxUtil = (path:string, password:string):Promise<Pkcs12ReadResult>=>{
+//   return new Promise((resolve, reject)=>{
+//     pem.readPkcs12(path, { p12Password: password }, (err:any, res:Pkcs12ReadResult) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(res);
+//     });
+//   });
+// };
 
 
 const parsePfx = async(path:string, password:string = ''):Promise<Record<'privateKey'| 'certificate', string>>=>{
   if (!path) throw new Error('The path cannot be empty');
-  const { key, cert } = await parsePfxUtil(path, password);
+  const { pemKey, pemCertificate } = p12.getPemFromP12(path, password);
   return {
-    privateKey: key,
-    certificate: cert
+    privateKey: pemKey,
+    certificate: pemCertificate
   };
 };
 
